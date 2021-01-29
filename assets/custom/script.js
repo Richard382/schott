a5d1e70e55af69e2804296fd86f50766eed7bdad
Richard <richard1997117@outloook.com>
2021-01-30 05:22:01 +1100

    30-01-2021

--- assets/custom/script.js
+++ assets/custom/script.js
@@ -1,4 +1,12 @@
+const modal = document.getElementById("settingModal");
     function showSettingModal()
     {
-        alert('show modal');
+        // modal.style.display = "block";
     }
+    $('.closeModal').on('click', function () {
+        modal.style.display = "none";
+    });
+    $(document).keydown(function (e) {
+        var code = e.keyCode || e.which;
+        if (code == 27) modal.style.display = "none";
+    });
